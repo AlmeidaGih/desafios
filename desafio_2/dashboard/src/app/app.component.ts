7681d96ffc9fedbdcc8dcb39eead73595163a9a9
Giovanna Almeida <giovanna.almeida.sousa1@gmail.com>
2022-01-25 16:45:33 -0300

    Side bar criada

--- desafio_2/dashboard/src/app/app.component.ts
+++ desafio_2/dashboard/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './side-bar/app.side.bar.html',
+  styleUrls: ['./side-bar/app.side.bar.css']
 })
+
 export class AppComponent {
   title = 'dashboard';
 }
