d6fae098ec5a32bce831f437fd83073434c56b60
Giovanna Almeida <giovanna.almeida.sousa1@gmail.com>
2022-01-25 19:42:34 -0300

    Projeto reconstruído

--- desafio_2/dashboard/src/app/app.component.ts
+++ desafio_2/dashboard/src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 
-
-
 @Component({
   selector: 'app-root',
-  templateUrl: './side-bar/app.side.bar.html',
-  styleUrls: ['./side-bar/app.side.bar.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
   title = 'dashboard';
 }
--- desafio_2/dashboard/src/app/app.module.ts
+++ desafio_2/dashboard/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
