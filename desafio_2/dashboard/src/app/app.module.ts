22fcff02d8f6a21bf0a3543eaaf374cbe7eae361
Giovanna Almeida <giovanna.almeida.sousa1@gmail.com>
2022-01-26 13:50:10 -0300

    Recriação do projeto com scss
    adicionando também, os ícones que serão utilizados

--- desafio_2/dashboard/src/app/app.module.ts
+++ desafio_2/dashboard/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SidebarComponent,
-    NavbarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
